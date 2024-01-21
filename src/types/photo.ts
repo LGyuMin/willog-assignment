49fceb0e1a84d4b1e5cd6d59d75f6b112b1d7c43
규.이규민 <gmlee@lunasoft.co.kr>
2024-01-21 16:42:33 +0900

    미사용 타입 제거

--- src/types/photo.ts
+++ src/types/photo.ts
@@ -9,22 +9,3 @@ export interface IPhoto extends Full {
     }[]
 
 }
-
-export interface Photo {
-    id: number;
-    width: number;
-    height: number;
-    alt_description: string;
-    urls: { 
-        large: string; 
-        regular: string; 
-        raw: string; 
-        small: string;
-        thumb: string;
-    };
-    color: string | null;
-    user: {
-        username: string;
-        name: string;
-    };
-};
